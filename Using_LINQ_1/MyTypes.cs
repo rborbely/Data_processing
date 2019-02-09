c4fad6afff46a5ab70d598adbc09a9236070a8ca
Roland Borbély <borbely.roland@gmail.com>
2019-02-10 00:15:30 +0100

    First implementation of demo

---
+++ Using_LINQ_1/MyTypes.cs
@@ -0,0 +1,66 @@
+﻿/***************************************************************/
+/* Author: Roland Borbély                                      */
+/* Assembly:  Lambda_Exp_1                                     */
+/* File: MyTypes.cs                                            */
+/*                                                             */
+/* This file contains type definitions in relation to this     */
+/* application.                                                */
+/***************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Exp_1
+{
+    enum gender
+    {
+        Male,
+        Female
+    }
+
+    class Person 
+    {
+        private int age;
+        private string name;
+        private string job;
+        private string country;
+        private bool married;
+        private gender gender;
+
+        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set => name = value; }
+        public string Job { get => job; set => job = value; }
+        public string Country { get => country; set => country = value; }
+        public bool Married { get => married; set => married = value; }
+        internal gender Gender { get => gender; set => gender = value; }
+
+        public int Age2
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public Person(String name, int age, gender gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.job = "";
+            this.country = "Hungary";
+            this.married = false;
+            this.gender = gender;
+        }
+    }
+
+    
+}
---
+++ Using_LINQ_1/Program.cs
@@ -0,0 +1,73 @@
+﻿/***************************************************************/
+/* Author: Roland Borbély                                      */
+/* Title:  Lambda_Exp_1                                        */
+/*                                                             */
+/* This is a simple example program to demonstrate my way of   */
+/* design and implementation of using lambda expressions. This */
+/* file is the main of the application                         */
+/***************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+
+namespace Lambda_Exp_1
+{
+       
+    class Program
+    {
+        /* Create global variable */
+        static List<Person> people = new List<Person>();
+
+        static void Main(string[] args)
+        {
+            /* Fill up with some dummy data */
+            people.Add(new Person("Tom", 33, gender.Male));
+            people.Add(new Person("Nick", 28, gender.Male));
+            people.Add(new Person("Jenny", 18, gender.Female));
+            people.Add(new Person("Louise", 45, gender.Female));
+            people.Add(new Person("Frank", 67, gender.Male));
+
+
+            Console.WriteLine("Let's, list the ladies!");
+
+            List<Person> ladies = people.FindAll(prs => prs.Gender == gender.Female);
+
+            ladies.ForEach((prs) => Console.WriteLine("{0} is a {1} and is {2}.",
+                                                     prs.Name,
+                                                     prs.Gender,
+                                                     prs.Age));
+
+            Console.WriteLine("\nNow  list the men!");
+
+            /* First find all the men...  */
+            List<Person> men = people.FindAll(prs => prs.Gender == gender.Male);
+            /* Write them on output */
+            men.ForEach((prs) => Console.WriteLine("{0} is a {1} and is {2}.",
+                                         prs.Name,
+                                         prs.Gender,
+                                         prs.Age));
+
+
+            /*  Now the same with a shorter call */
+            Console.WriteLine("\n... and again with a more simple form!");
+            people.ForEach(prs =>
+            {
+                if (prs.Gender == gender.Male)
+                {
+                    Console.WriteLine("{0} is a {1} and is {2}.",
+                                        prs.Name,
+                                        prs.Gender,
+                                        prs.Age);
+                }
+
+            });
+
+            /* Added to prevent closing application. */
+            Console.Read();
+        }
+    }
+}
